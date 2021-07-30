5e45ed6fd3606f39ca7ab9add37217c5d821e77c
Masahiro Nakagawa <hirohirox@gmail.com>
2021-07-30 10:25:58 +0900

    use global state esp recoil

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
+// import { useLocation } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { userState } from "../../store/userState";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -16,10 +20,20 @@ const users = [...Array(10).keys()].map((val) => {
   };
 });
 export const Users = () => {
+  // const { state } = useLocation();
+  // const isAdmin = state ? state.isAdmin : false;
+
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+
+  const onClickSwitch = () => {
+    setUserInfo({ isAdmin: !userInfo.isAdmin });
+  };
+
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <SecondaryButton onClick={onClickSwitch}>切替</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
